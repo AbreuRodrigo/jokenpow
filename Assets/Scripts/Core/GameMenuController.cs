66b8630015a082399c398a8eb2955cbf3dd80b98
RodrigoAbreu <rodrigojava3d@gmail.com>
2015-01-20 22:31:01 -0200

    Adding keystore and correcting a lot of mistakes

--- Assets/Scripts/Core/GameMenuController.cs
+++ Assets/Scripts/Core/GameMenuController.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
 
 public class GameMenuController : MonoBehaviour {
+
+	bool isAuth = false;
 
 	void Update () {
 		if(Input.GetKey(KeyCode.Escape)){
@@ -11,5 +15,21 @@ public class GameMenuController : MonoBehaviour {
 
 	public static void LoadGamePlay(){
 		Application.LoadLevel("GamePlaySingle");
+	}
+
+	void OnGUI(){
+		if(GUI.Button(new Rect(10, 10, 100, 50), "Connect")){
+			PlayGamesPlatform.Activate();
+			PlayGamesPlatform.Instance.Authenticate((bool success) =>{
+				isAuth = success;
+			});
+		}
+
+		GUI.color = Color.black;
+		GUI.Label(new Rect(10, 100, 100, 50), PlayGamesPlatform.Instance.IsAuthenticated() + " " + isAuth);
+
+		if(GUI.Button(new Rect(10, 190, 100, 50), "Leaderboard")){
+			PlayGamesPlatform.Instance.ShowLeaderboardUI("CgkIh7jKwaEKEAIQBg");
+		}
 	}
 }
